99e7dac44d257d0629285e3cf4de3bb620e2533e
Yahya Al-Ademi <yahyaalademi84@gmail.com>
2025-06-17 08:52:21 +0200

    Update constants, questionView, and data modules

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,5 +1,8 @@
-import { ANSWERS_LIST_ID } from '../constants.js';
-import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
+import {
+  ANSWERS_LIST_ID,
+  NEXT_QUESTION_BUTTON_ID,
+  SKIP_QUESTION_BUTTON_ID
+} from '../constants.js';
 
 /**
  * Create a full question element
@@ -15,8 +18,11 @@ export const createQuestionElement = (question) => {
     <ul id="${ANSWERS_LIST_ID}">
     </ul>
 
-    <button id="${NEXT_QUESTION_BUTTON_ID}">
-      Next question
+     <button id="${NEXT_QUESTION_BUTTON_ID}" style="display:none;">
+    Next question
+  </button>
+    <button id="${SKIP_QUESTION_BUTTON_ID}">
+      Skip & Show Answer
     </button>
   `;
 
