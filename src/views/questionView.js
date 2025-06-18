9992d7869bfac9a5a97ff8ef2306f9ffbb25a9c1
Yahya Al-Ademi <yahyaalademi84@gmail.com>
2025-06-18 17:50:14 +0200

    Update constants, data, and questionView  with Prettier correction

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,22 +1,31 @@
-import { ANSWERS_LIST_ID } from '../constants.js';
-import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
+import {
+  ANSWERS_LIST_ID,
+  NEXT_QUESTION_BUTTON_ID,
+  SKIP_QUESTION_BUTTON_ID,
+  RESET_QUESTION_BUTTON_ID,
+} from '../constants.js';
+// import { quizData } from '../data.js';
+// USER_INTERFACE_ID
 
-/**
- * Create a full question element
- * @returns {Element}
- */
 export const createQuestionElement = (question) => {
   const element = document.createElement('div');
 
-  // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
     <h1>${question}</h1>
 
     <ul id="${ANSWERS_LIST_ID}">
     </ul>
 
-    <button id="${NEXT_QUESTION_BUTTON_ID}">
+    <button id="${NEXT_QUESTION_BUTTON_ID}" style="display:none;">
       Next question
+    </button>
+
+    <button id="${SKIP_QUESTION_BUTTON_ID}">
+      Skip & Show Answer
+    </button>
+
+    <button id="${RESET_QUESTION_BUTTON_ID}" style="display:none;">
+      Reset
     </button>
   `;
 
