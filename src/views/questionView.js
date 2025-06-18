8a538aac5b056c89e20f5670cebbe3e37882017c
VLADISLAV N. <vlnachikov@gmail.com>
2025-06-18 20:43:03 +0200

    Revert "Update constants, questionView, and data modules"

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,29 +1,22 @@
-import {
-  ANSWERS_LIST_ID,
-  NEXT_QUESTION_BUTTON_ID,
-  SKIP_QUESTION_BUTTON_ID,
-  RESET_QUESTION_BUTTON_ID,
-} from '../constants.js';
+import { ANSWERS_LIST_ID } from '../constants.js';
+import { NEXT_QUESTION_BUTTON_ID } from '../constants.js';
 
+/**
+ * Create a full question element
+ * @returns {Element}
+ */
 export const createQuestionElement = (question) => {
   const element = document.createElement('div');
 
+  // I use String.raw just to get fancy colors for the HTML in VS Code.
   element.innerHTML = String.raw`
     <h1>${question}</h1>
 
     <ul id="${ANSWERS_LIST_ID}">
     </ul>
 
-    <button id="${NEXT_QUESTION_BUTTON_ID}" style="display:none;">
+    <button id="${NEXT_QUESTION_BUTTON_ID}">
       Next question
-    </button>
-
-    <button id="${SKIP_QUESTION_BUTTON_ID}">
-      Skip & Show Answer
-    </button>
-
-    <button id="${RESET_QUESTION_BUTTON_ID}" style="display:none;">
-      Reset
     </button>
   `;
 
