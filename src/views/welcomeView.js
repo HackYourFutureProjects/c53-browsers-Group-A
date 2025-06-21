2521495986197ba76c4a044c1ea464e83e1881fc
Anastasiia Kuzko <anastasiyabetsko0987@gmail.com>
2025-06-21 21:46:59 +0200

    Improved the welcome page

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -6,9 +6,14 @@ import { START_QUIZ_BUTTON_ID } from '../constants.js';
  */
 export const createWelcomeElement = () => {
   const element = document.createElement('div');
+  element.classList.add('welcome-container');
+
   element.innerHTML = String.raw`
-    <h1>Welcome</h1>
-    <button id="${START_QUIZ_BUTTON_ID}">start quiz</button>
+  <h1 class="welcome-title">Welcome to JS Quiz!</h1>
+  <p class="welcome-description">
+  Test your JavaScript knowledge - answer questions and find out how pro you are!
+    </p>
+    <button id="${START_QUIZ_BUTTON_ID}" class="start-button">start quiz</button>
   `;
   return element;
 };
