91607d0b7b6fc4c82c632121557a2d416511e179
VLADISLAV NACHIKOV <vlnachikov@gmail.com>
2025-06-23 11:30:20 +0200

    Small fix

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -11,7 +11,7 @@ export const createWelcomeElement = () => {
   element.innerHTML = String.raw`
   <h1 class="welcome-title">Welcome to JS Quiz!</h1>
   <p class="welcome-description">
-  Test your JavaScript knowledge - answer questions and find out how pro you are!
+  Test your Agile knowledge - answer questions and find out how pro you are!
     </p>
     <button id="${START_QUIZ_BUTTON_ID}" class="start-button">start quiz</button>
   `;
